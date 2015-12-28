5fd8b5c6ccb8e258a87f3deb9e72f49dda3eb290
fengqi <feng.qi@msn.com>
2015-12-28 20:19:51 +0800

    portability version

--- chapter04/prompt.c
+++ chapter04/prompt.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy) - 1] = '\0';
+	return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline header */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
-
-	/* Print Version and Exit Information */
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 
 	while (1) {
-		fputs("lispy> ", stdout);
 
-		fgets(input, 2048, stdin);
+		/* Now in either case readline will be correctly defined */
+		char* input = readline("lispy> ");
+		add_history(input);
 
-		printf("No you're a %s", input);
+		printf("No you're a %s\n", input);
+		free(input);
 	}
-
 	return 0;
 }
 
