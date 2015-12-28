188dcb991db54127f016cacc6d578fdee72681bc
fengqi <feng.qi@msn.com>
2015-12-28 20:10:35 +0800

    chapter 4, windows version

---
+++ chapter04/prompt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Declare a buffer for user input of size 2048 */
+static char input[2048];
+
+int main(int argc, char** argv) {
+
+	/* Print Version and Exit Information */
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+
+	while (1) {
+		fputs("lispy> ", stdout);
+
+		fgets(input, 2048, stdin);
+
+		printf("No you're a %s", input);
+	}
+
+	return 0;
+}
+
